53eb0ff24a375ea363e06a9496c2141d012d17e4
Alex <forcablaugrana@gmail.com>
2022-06-07 11:06:06 +0300

    form validation added

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -23,7 +23,7 @@
 
             <form @submit.prevent="submit" action="#" class="mt-5">
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="name-input" class="mb-0">
                     Name
                     <span style="color: red">*</span>
@@ -31,16 +31,19 @@
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.name"
+                    v-model="v$.name.$model"
                     type="text"
                     class="form-control"
                     id="name-input"
                   />
+                  <span v-for="error in v$.name.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="email-input" class="mb-0">
                     E-mail
                     <span style="color: red">*</span>
@@ -48,21 +51,24 @@
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.email"
+                    v-model="v$.email.$model"
                     type="email"
                     class="form-control"
                     id="email-input"
                   />
+                  <span v-for="error in v$.email.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="phone-input" class="mb-0"> Phone </label>
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.phone"
+                    v-model="phone"
                     type="tel"
                     class="form-control"
                     id="phone-input"
@@ -71,7 +77,7 @@
               </div>
 
               <div class="form-group row textarea">
-                <div class="col col-12 d-flex justify-content-center">
+                <div class="col col-12 d-flex justify-content-start">
                   <label for="pmessage" class="mb-3 mt-3 text-center">
                     Your message
                     <span style="color: red">*</span>
@@ -79,21 +85,22 @@
                 </div>
                 <div class="col col-12">
                   <textarea
-                    v-model="form.message"
+                    v-model="v$.message.$model"
                     class="form-control"
                     name="message"
                     id="message"
                     rows="5"
                     placeholder="Leave your comments here"
                   ></textarea>
+                  <span v-for="error in v$.message.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="row">
                 <div class="col">
-                  <button
-                    type="submit" class="btn btn-outline-dark send-btn"
-                  >
+                  <button type="submit" class="btn btn-outline-dark send-btn">
                     Send us
                   </button>
                 </div>
@@ -110,21 +117,40 @@
 import NavBar from "@/components/NavBar.vue";
 import HeaderTitle from "@/components/HeaderTitle.vue";
 
+import useVuelidate from "@vuelidate/core";
+import { required, email, maxLength } from "@vuelidate/validators";
+import { helpers } from "@vuelidate/validators";
+import { minLength } from "../validators/minLength";
+
 export default {
   components: { NavBar, HeaderTitle },
+  setup() {
+    return { v$: useVuelidate() };
+  },
   data() {
     return {
-      form: {
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    };
+  },
+  validations() {
+    return {
+      name: { required },
+      email: { required, email },
+      phone: "",
+      message: {
+        required,
+        maxLength: maxLength(140),
+        minLength: helpers.withMessage("5 symbols minimum, please", minLength),
       },
     };
   },
   methods: {
-    submit() {
-      console.log(this.form);
+    async submit() {
+      const isFormCorrect = await this.v$.$validate();
+      if (!isFormCorrect) return;
     },
   },
 };
